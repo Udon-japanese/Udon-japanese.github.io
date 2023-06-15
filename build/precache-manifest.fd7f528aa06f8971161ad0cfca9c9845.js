587d810127465e5658704a07bc458027988de21a
Udon-japanese <odaniyoshito@gmail.com>
2023-06-15 21:14:32 +0900

    change

---
+++ build/precache-manifest.fd7f528aa06f8971161ad0cfca9c9845.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d60cb5db9199bdb787607f3c4b2dcd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2237694a28e67809a04b",
+    "url": "/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "f1fb857c822e79583d56",
+    "url": "/static/css/main.e8901382.chunk.css"
+  },
+  {
+    "revision": "2237694a28e67809a04b",
+    "url": "/static/js/2.0870e299.chunk.js"
+  },
+  {
+    "revision": "334a3166bc9976dda67650e8c6f88652",
+    "url": "/static/js/2.0870e299.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1fb857c822e79583d56",
+    "url": "/static/js/main.a92371ca.chunk.js"
+  },
+  {
+    "revision": "6074ff583ca711c31fe3",
+    "url": "/static/js/runtime-main.1dbfc099.js"
+  }
+]);
